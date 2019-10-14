c393fd881d76e69112a1dd521a9f593e9fa071dd
Oguz Bilgic <fisyonet@gmail.com>
2019-10-14 13:36:23 -0700

    Add no-restricted-modules rule to node.js (#4)
    We would like to prevent this modules being added as new dependencies. However if they are already part of the package.json we don't want our developers to go and refactor all the code, so this PR introduces a new warning using `no-restricted-modules` rule for `axios` and `request` packages.

--- node.js
+++ node.js
@@ -1,3 +1,5 @@
+const restrictedModulesMessage = 'If your PR introduces this module as a new dependency please use node-fetch instead; Otherwise do not try to refactor and ignore this warning.'
+
 module.exports = {
   env: {
     node: true,
@@ -7,6 +9,12 @@ module.exports = {
   ],
   rules: {
     // Allow _id, as its part of Mongo id
-    'no-underscore-dangle': ['error', { allow: ['_id'] }]
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'no-restricted-modules': ['warn', {
+      paths: [
+        { name: 'request', message: restrictedModulesMessage },
+        { name: 'axios', message: restrictedModulesMessage }
+      ]
+    }]
   }
 }
