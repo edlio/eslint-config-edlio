f9056da0e48a9738924f77eb81913ab07e07c2e4
Oguz Bilgic <fisyonet@gmail.com>
2019-10-14 12:58:36 -0700

    Simplify restrictedModulesMessage

--- node.js
+++ node.js
@@ -1,6 +1,4 @@
-const restrictedModuleMessage = module => (
-  `${module} module is restricted. If your PR introduces this module as a new dependency please use node-fetch instead; Otherwise do not try to refactor and ignore this warning.`
-)
+const restrictedModuleMessage = 'If your PR introduces this module as a new dependency please use node-fetch instead; Otherwise do not try to refactor and ignore this warning.'
 
 module.exports = {
   env: {
@@ -14,8 +12,8 @@ module.exports = {
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
     'no-restricted-modules': ['warn', {
       paths: [
-        { name: 'request', message: restrictedModuleMessage('request') },
-        { name: 'axios', message: restrictedModuleMessage('axios') }
+        { name: 'request', message: restrictedModulesMessage },
+        { name: 'axios', message: restrictedModulesMessage }
       ]
     }]
   }
