f3fd0bd60c421513edef86fd067d0c46f1f234f2
Markas Klapovščiuk <48568793+markkkkas@users.noreply.github.com>
2024-02-08 15:38:37 +0200

    feat: Improve React, introduce TypeScript rules (#18)
    Resolves: https://edlioinc.atlassian.net/browse/CMSDV-617

---
+++ react-strict.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['edlio/react'],
+  rules: {
+    semi: 'error',
+    complexity: 'error',
+    curly: 'error',
+    'id-length': 'error',
+    'max-depth': 'error',
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+        ignorePattern: '^import .*',
+      },
+    ],
+    'max-lines': ['error', 500],
+    'max-params': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-nested-ternary': 'error',
+    'no-else-return': 'error',
+    'no-implicit-coercion': 'error',
+  },
+};
